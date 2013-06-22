32c7e63137e2b951eb7993deba29fe0148fe0350
Todd Gamblin <tgamblin@llnl.gov>
2013-06-22 10:48:52 -0700

    Updates

--- home/.bash.d/adept.sh
+++ home/.bash.d/adept.sh
@@ -1,1 +1,3 @@
-use -q .adept
+if [[ -n "$DK_ROOT" ]]; then
+    use -q .adept
+fi
--- home/.bash.d/atlassian.sh
+++ home/.bash.d/atlassian.sh
@@ -1,6 +1,7 @@
 export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 
 export ATLAS_HOME=$HOME/src/atlassian-plugin-sdk-4.1
+export M2_HOME=$ATLAS_HOME/apache-maven
+
 pathadd $ATLAS_HOME/bin
-pathadd $ATLAS_HOME/apache-maven/bin
-alias mvn=atlas-mvn
+pathadd $M2_HOME/bin
