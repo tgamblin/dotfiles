6026a9cf54f189946a1ef753bbdaab9250fbc2f8
Todd Gamblin <tgamblin@llnl.gov>
2015-05-21 16:37:50 -0700

    safer atlassian config.

--- home/.bash.d/atlassian.sh
+++ home/.bash.d/atlassian.sh
@@ -1,8 +1,9 @@
-export JAVA_HOME=$(/usr/libexec/java_home -v1.8)
+if [ -d /usr/libexec/java_home ]; then
+    export JAVA_HOME=$(/usr/libexec/java_home -v1.8)
+    export ATLAS_HOME=/usr/share/atlassian-plugin-sdk
+    export M2_HOME=$ATLAS_HOME/apache-maven
 
-export ATLAS_HOME=/usr/share/atlassian-plugin-sdk
-export M2_HOME=$ATLAS_HOME/apache-maven
-
-pathadd $JAVA_HOME/bin
-pathadd $ATLAS_HOME/bin
-pathadd $M2_HOME/bin
+    pathadd $JAVA_HOME/bin
+    pathadd $ATLAS_HOME/bin
+    pathadd $M2_HOME/bin
+fi
